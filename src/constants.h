c0e34d1147212da24cb4e04cd48256fb6f1c7bae
lasiqueira <lucas.asiq@gmail.com>
2026-07-11 01:28:11 +0100

    refactor

--- src/constants.h
+++ src/constants.h
@@ -6,7 +6,6 @@ constexpr int CPU_FREQUENCY = 4194304;        // 4.194304 MHz (cycles per second
 constexpr double FRAME_RATE = 59.7;           // ~59.7 Hz (frames per second)
 constexpr int CYCLES_PER_FRAME = static_cast<int>(CPU_FREQUENCY / FRAME_RATE);  // ~70224 cycles
 constexpr int   SAMPLE_RATE        = 44100;
-constexpr float CYCLES_PER_SAMPLE  = static_cast<float>(CPU_FREQUENCY) / SAMPLE_RATE; 
 
 // LCD timing constants
 constexpr int CYCLES_PER_SCANLINE = 456; // 456 cycles per scanline
